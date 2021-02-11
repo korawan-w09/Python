69053de80f2faab6d12b2eb605f8800d79638705
Korawan Kamesri <korawan.ka@kkumail.com>
2021-02-11 22:46:05 +0700

    week1

---
+++ week1 test1.py
@@ -0,0 +1,8 @@
+name = input('What is you name?\n')
+surname = input('What is you surname?\n')
+collegeyears = input('What is you college year ?\n')
+code = input('what is you code ?\n')
+print('My name is :%s.' %name)
+print('My surname is :%s.' %surname)
+print('My college years is :%s.' %collegeyears)
+print('My code is :%s.' %code)
