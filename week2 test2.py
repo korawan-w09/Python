ce1ec1d36dd0a2850bfafd295e13e5875104929e
Korawan Kamesri <korawan.ka@kkumail.com>
2021-02-19 14:43:05 +0700

    week2

---
+++ week2 test2.py
@@ -0,0 +1,6 @@
+#Korawan Kamesri 633050433-8
+#Day Converter Program
+x=int (input("Number of Days-->"))
+print("Days--> Hour :",24*x,"Hour")
+print("Days--> Minutes :",60*24*x,"Minutes")
+print("Days--> Seconds :",60*60*24*x,"Seconds")
